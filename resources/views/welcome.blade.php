f6a79ea9b52f574b98e3e4147e9f180b69187142
datomaluta <datomaluta@gmail.com>
2022-11-08 18:02:54 +0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+    </head>
+    <body class="bg-red-500">
+        <h1>Hello World</h1>
+    </body>
+</html>
