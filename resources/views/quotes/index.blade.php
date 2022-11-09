01693268884902fe716ae2b74fd23516e21eb816
datomaluta <datomaluta@gmail.com>
2022-11-09 20:08:03 +0400

    feat(mainpage design): style layout and quote page

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-    <h1>{{$quote->text}}</h1>
+    <div class="flex flex-col items-center mt-56">
+        <div class="max-w-[43.8rem] max-h-[24.13rem] mb-16">
+            <img class="w-full h-full rounded-[0.625rem]" src="https://cdn.pixabay.com/photo/2018/01/12/10/19/fantasy-3077928__480.jpg" alt="quoteim">
+        </div>
+        <h1 class="text-white text-5xl text-center leading-[3.35rem] mb-28">"{{$quote->text}}"</h1>
+
+        <a class="text-white text-5xl text-center underline leading-[3.35rem]" href="#">The Movie Title</a>
+    </div>
+    
 </x-layout>
