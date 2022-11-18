8b3e3bcdc51490b9efd36f49493cacc7eac6ed08
datomaluta <datomaluta@gmail.com>
2022-11-18 12:59:42 +0400

    fix(quote-migration): create new migration for constrained

--- database/migrations/2022_11_09_075302_create_quotes_table.php
+++ database/migrations/2022_11_09_075302_create_quotes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
 	{
 		Schema::create('quotes', function (Blueprint $table) {
 			$table->id();
-			$table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+			$table->foreignId('movie_id');
 			$table->string('thumbnail');
 			$table->text('text');
 			$table->timestamps();
