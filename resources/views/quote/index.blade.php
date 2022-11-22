62bc4b5d7a3e61f63505f52c15d994875810a68b
datomaluta <datomaluta@gmail.com>
2022-11-22 18:57:43 +0400

    fix(local-images): use faker instead of static images

--- resources/views/quote/index.blade.php
+++ resources/views/quote/index.blade.php
@@ -1,10 +1,14 @@
 <x-layout>
     @if ($quote)
         <div class="flex flex-col items-center mt-56">
-            <div class="max-w-[43.8rem] max-h-[24.13rem] mb-16">
-                <img class="w-full h-full rounded-[0.625rem]" src="{{ asset('storage/' . $quote->thumbnail) }}"
-                    alt="quoteim">
+            <div class="w-[43.8rem] h-[24.13rem] mb-16">
+                @if (Str::startsWith($quote->thumbnail, 'thumbnails'))
+                    <img class="w-full h-full rounded-[0.625rem] object-cover" src="{{asset('storage/' . $quote->thumbnail)}}" alt="quoteimggg">
+                @else
+                    <img class="w-full h-full rounded-[0.625rem] object-cover" src="{{$quote->thumbnail}}" alt="quoteimggg">
+                @endif
             </div>
+
             <h1 class="text-white text-5xl text-center leading-[4.35rem] mb-28 px-24 ">"{{ $quote->text }}"</h1>
 
             <a class="text-white text-5xl text-center underline leading-[3.35rem]"
